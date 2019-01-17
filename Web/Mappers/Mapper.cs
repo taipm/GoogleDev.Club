dda1a24032d55c3458a2c141455a2da14ff6a9bb
Phan Minh Tai <taipm@workcard.vn>
2019-01-17 10:44:00 +0700

    Commit

---
+++ Web/Mappers/Mapper.cs
@@ -0,0 +1,100 @@
+﻿using CafeT.Html;
+using CafeT.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web.Models;
+using Web.ModelViews;
+
+namespace Web.Mappers
+{
+    public static class Mapper
+    {
+        public static ArticleModel EntryToArticle(dynamic entryItem)
+        {
+            ArticleModel model = new ArticleModel();
+            model.Id = int.Parse(GetFieldValue(entryItem, "Id"));
+            model.Title = GetFieldValue(entryItem, "Title");
+            model.Description = GetFieldValue(entryItem, "Description");
+            model.ContentUrl = GetFieldValue(entryItem, "ContentUrl");
+            model.Tags = GetFieldValue(entryItem, "Tags");
+            model.CreatedBy = GetFieldValue(entryItem, "CreatedBy");
+            model.CreatedDate = GetFieldValue(entryItem, "CreatedDate");
+            model.IsPublished = bool.Parse(GetFieldValue(entryItem, "IsPublished"));
+            model.MetaDescription = GetFieldValue(entryItem, "MetaDescription");
+            model.MetaKeywords = GetFieldValue(entryItem, "MetaKeywords");
+
+            return model;
+        }
+        #region View
+        public static ArticleView ArticleToView(ArticleModel model)
+        {
+            ArticleView view = new ArticleView();
+            view.Id = model.Id;
+            view.CreatedDate = model.CreatedDate;
+            view.Title = GetRawText(model.Title);
+            view.Description = GetRawText(model.Description);
+            if (!model.Content.IsNullOrEmptyOrWhiteSpace())
+            {
+                if (model.Content.IsHtmlString())
+                {
+                    view.Content = DecorHtml(model.Content);
+                }
+            }
+            view.Tags = model.Tags.Split(new char[] { ',', ';' }, StringSplitOptions.None);
+            view.IsPublished = model.IsPublished;
+            view.CreatedBy = model.CreatedBy;
+            view.MetaKeywords = model.Tags + ";" + model.MetaKeywords;
+            view.MetaDescription = model.MetaDescription;
+
+            return view;
+        }
+
+        public static List<ArticleView> ArticlesToViews(List<ArticleModel> models)
+        {
+            List<ArticleView> views = new List<ArticleView>();
+            foreach (var model in models)
+            {
+                //var view = ArticleToView(model)
+                views.Add(ArticleToView(model));
+            }
+            return views;
+        }
+        #endregion
+        public static string GetFieldValue(dynamic item, string fieldName)
+        {
+            try
+            {
+                string value = (string)item["gsx$" + fieldName.ToLower()]["$t"].ToString();
+                return value.DeleteFirst("\"").DeleteLastest("\"");
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        /// <summary>
+        /// Because from Google Sheets, text has two char " in begin and end, must to remove
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string GetRawText(string text)
+        {
+            string output = text; //string output = text.DeleteFirst("\"").DeleteLastest("\"");
+            output = output.Replace("\n", "<br />");
+            output = output.Replace("\\", "\"");
+            return output;
+        }
+
+        public static string DecorHtml(string htmlText)
+        {
+            string html = string.Empty;
+            html = htmlText.Replace("\n", "<br />");
+            html = html.Replace("\\", "\"");
+            html = htmlText.ResizeImages("100%", "auto");
+
+            return html;
+        }
+    }
+}

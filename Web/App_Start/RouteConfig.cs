db83a124507c00b9c5d43acc50949b1d61499e36
Phan Minh Tai <taipm@workcard.vn>
2019-01-17 11:13:08 +0700

    Add SEO

--- Web/App_Start/RouteConfig.cs
+++ Web/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Web.Helpers;
 
 namespace Web
 {
@@ -12,6 +13,11 @@ namespace Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.Add("ArticlesDetails",
+                new SeoFriendlyRoute("articles/details/{id}",
+                new RouteValueDictionary(new { controller = "Articles", action = "Details" }),
+                new MvcRouteHandler()));
 
             routes.MapRoute(
                 name: "Default",
